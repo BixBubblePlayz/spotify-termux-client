06519b1b334b416cdf6f9c5d3bf09a07c7bf7bfc
BixBubblePlayz <bixbubbleplayz@agentmail.to>
2026-04-14 18:40:03 +0000

    Fix Express 5 catch-all route

--- server/index.js
+++ server/index.js
@@ -186,6 +186,6 @@ app.get('/api/me', authRequired, async (_req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('*', (_req, res) => res.sendFile(path.join(__dirname, '..', 'build', 'index.html')));
+app.use((_req, res) => res.sendFile(path.join(__dirname, '..', 'build', 'index.html')));
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server listening on http://0.0.0.0:${PORT}`));
